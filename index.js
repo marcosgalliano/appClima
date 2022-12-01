750744a2568d36dec17470fab5f77a492c8fba49
marcos galliano <marquitosgalliano@hotmail.com>
2022-12-01 18:09:53 -0300

    sunrise sunset puestos

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const obtenerClima = () => {
 
 const consultarAPI= async(ciudad) =>{
     const apiKEY="bf591ca3aa502b2a1c70a03ecc1713ce";
-    const url=`https://api.openweathermap.org/data/2.5/weather?q=${ciudad},AR&appid=${apiKEY}&units=metric`
+    const url=`https://api.openweathermap.org/data/2.5/weather?q=${ciudad},AR&appid=${apiKEY}&units=metric&lang=es`
     console.log(url);
     const respuesta = await fetch(url);
     const resultado = await respuesta.json();
@@ -22,10 +22,29 @@ const consultarAPI= async(ciudad) =>{
         mostrarError("msj-error","NO HAY RESULTADOS");
         return;
     }
-    const {name ,main, weather,dt}=resultado;
+    const {name ,main, weather,sys}=resultado;
     if(!name) return null;
 
     let divResultado=document.getElementById("resultado");
+
+    //SUNRISE SUNSET
+    let sunset = sys.sunset;
+    let sunrise = sys.sunrise;
+
+    let unix_sunrise = sunrise;
+    let unix_sunset = sunset;
+
+    let date = new Date (unix_sunrise * 1000);             //1=sunrise 2=sunset
+    let date2 = new Date (unix_sunset * 1000);
+
+    let hours = date.getHours();
+    let hours2 = date2.getHours();
+
+    let minutes = "0" + date.getMinutes();
+    let minutes2 = "0" + date2.getMinutes();
+
+    let conversion =  hours + ':' + minutes.substr(-2);
+    let conversion2 =  hours2 + ':' + minutes2.substr(-2);
 
     //DATOS WEATHER
     //TEMPERATURA
@@ -43,21 +62,37 @@ const consultarAPI= async(ciudad) =>{
     let minima = parseFloat(main.temp_min-9,10);
     let minimaRedondeada = Math.round(minima);
 
+    //DESCRIPCION
+    let descripcion = weather[0].description;
+    let descripcionMay = descripcion.toUpperCase();
+    
+    //ICONO
     let icono = weather[0].icon;
 
     divResultado.innerHTML=`
                         <div class="div_carta">
                             <h1>Argentina, ${name}</h1>
                             <div class="datos">
-                                <p class="temperatura">
-                                    ${temperaturaRedondeada}&#x2103
-                                </p>
+                                <div class="temperatura">
+                                    <p>
+                                        ${temperaturaRedondeada}&#x2103
+                                    </p>
+                                    <h3>${descripcionMay}</h3>
+                                    <div class="max_min">
+                                        <h4>Max: ${maximaRedondeada}&#x2103</h4> 
+                                        <h4>Min: ${minimaRedondeada}&#x2103</h4>
+                                    </div>
+                                </div>    
                                 <img src="./assets/${icono}.png" alt="clima">
                                 <p class="sensacion">
                                 Sensacion Termica: <br>${sensacionRedondeada}&#x2103
                                 </p>
-                                <div class="maxima_minima">
-                                <p>${maximaRedondeada}-${minimaRedondeada}</p>
+                                <div class="sunset_sunrise">
+                                    <h3>Salida y Puesta del Sol</h3>
+                                    <div class="sunset_sunrise_Result">
+                                        <h4>Salida del Sol:<br>${conversion} Am</h4>
+                                        <h4>Puesta del Sol:<br>${conversion2} Pm</h4>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -69,14 +104,6 @@ const mostrarError = (elemento, mensasje) =>{
     divError.innerHTML=`<p class="btn btn-danger">${mensasje}</p>`;
     setTimeout(()=> { divError.innerHTML='';}, 2500)
 }
-
-
-
-
-
-
-
-
 
 //RELOJ !!!!!!!!!
 const time = document.getElementById("time");
